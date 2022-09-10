14928b9a88c9dc08e811fcdb5230110028bfc6a6
ueki-yuto <ueki-yuto@dmm.com>
2022-09-10 17:25:16 +0900

    validation追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 func main() {
 	e := echo.New()
 
+	e.Validator = new(CustomValidator)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
@@ -18,9 +20,37 @@ func main() {
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
+type CustomValidator struct{}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if v, ok := i.(validation.Validatable); ok {
+		return v.Validate()
+	}
+	return nil
+}
+
 type User struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
+}
+
+func (u *User) Validate() error {
+	return validation.ValidateStruct(u,
+		validation.Field(&u.Name, validation.Required),
+		validation.Field(&u.Email, validation.Required),
+	)
+}
+
+type ValidationError struct {
+	Field  string `json:"field"`
+	Reason string `json:"reason"`
+}
+
+func NewValidationError(field string, reason string) ValidationError {
+	return ValidationError{
+		Field:  field,
+		Reason: reason,
+	}
 }
 
 func getUser(c echo.Context) error {
@@ -36,5 +66,14 @@ func postUser(c echo.Context) error {
 	if err := c.Bind(u); err != nil {
 		return err
 	}
+	if err := c.Validate(u); err != nil {
+		errs := err.(validation.Errors)
+		var validationErrors []ValidationError
+		for k, err := range errs {
+			validationErrors = append(validationErrors, NewValidationError(k, err.Error()))
+		}
+		return c.JSON(http.StatusBadRequest, validationErrors)
+	}
+
 	return c.JSON(http.StatusOK, u)
 }
